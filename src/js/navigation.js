1070d1f2f51936d61cb29ddace27f2a39b9e3510
andresh83 <hernandez.andres.d@gmail.com>
2017-04-15 13:54:17 +1000

    navigation single page smooth scrolling and transition

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -3,6 +3,32 @@
    Navigation
    ========================================================================== */
 
-$('.navbar-toggle').on ('click', function() {
-	$('.navbar').toggleClass('open'); 
+$(document).ready( function () {
+	$('.navbar-toggle, .navbar-nav a').on ('click', function (e) {
+		e.preventDefault(); 
+		collapseNavBar(); 
+	});
+	
+	function collapseNavBar() {
+		$('.navbar').toggleClass('open'); 	
+	}; 
+
+})
+// =====================================================================
+// Smooth scrolling to anchor in a page 
+// @url: https://css-tricks.com/snippets/jquery/smooth-scrolling/
+// =====================================================================
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top -80
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });

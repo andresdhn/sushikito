2949a8310a4bef6ecd0a1eff048afb7e275dc9c6
Andres Hernandez <hernandez.andres.d@gmail.com>
2017-05-17 14:22:21 +1000

    Section hegiht revised draft#1 + reduce instagram posts

--- src/js/insta.js
+++ src/js/insta.js
@@ -7,7 +7,7 @@
         get: 'user',
         userId: '1488069419',
         accessToken: '1488069419.b773045.a708a07d1c4c40a981b955dae64e4e81',
-        limit:6,
+        limit:4,
         resolution: 'low_resolution',
         template: '<div class="grid-cell"><img src="{{image}}" alt=""/></div>'
     });

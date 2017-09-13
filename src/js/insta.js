cfd18f5775951177ad3f5f96de214395deed9c71
Andres Hernandez <hernandez.andres.d@gmail.com>
2017-09-13 11:36:15 +1000

    Carousel boxes with Instagram caption text

--- src/js/insta.js
+++ src/js/insta.js
@@ -10,7 +10,7 @@ var feed = new Instafeed({
     accessToken: '1488069419.b773045.a708a07d1c4c40a981b955dae64e4e81',
     limit: 12,
     resolution: 'low_resolution',
-    template: '<div class="carousel-cell" style="background-image:url({{image}})"></div>',
+    template: '<div class="carousel-cell" style="background-image:url({{image}})"><div class="cell-container"><div class="cell-textbox"><p>{{caption}}</p></div></div></div>',
     after: function () {
         var flickity = require('flickity');
         var flickityOptions = {'contain': true, 'wrapAround': true };

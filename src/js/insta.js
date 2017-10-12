74431bc2dbe9f0be6112f59f7c13a59dab64ed0e
Andres Hernandez <hernandez.andres.d@gmail.com>
2017-10-13 10:01:09 +1100

    Intafeed new section title + text--tilted effect + remove filter for instafeed js > not working

--- src/js/insta.js
+++ src/js/insta.js
@@ -3,22 +3,21 @@
    Instagram
    ========================================================================== */
 
-
-    var Instafeed = require("instafeed.js");
-    var feed = new Instafeed({
-        get: 'user',
-        userId: '1488069419',
-        accessToken: '1488069419.b773045.a708a07d1c4c40a981b955dae64e4e81',
-        limit: 12,
-        resolution: 'low_resolution',
-        template: '<div class="carousel-cell" style="background-image:url({{image}})"><div class="cell-container"><div class="cell-textbox"><p>{{caption}}</p></div></div></div>',
-        after: function () {
-            var flickity = require('flickity');
-            var flickityOptions = {'contain': true, 'wrapAround': true };
-            var gallery = new flickity(document.getElementById('instafeed'), flickityOptions);        
-        }
-    });
-    feed.run();
+var Instafeed = require("instafeed.js");
+var feed = new Instafeed({
+    get: 'user',
+    userId: '1488069419',
+    accessToken: '1488069419.b773045.a708a07d1c4c40a981b955dae64e4e81',
+    limit: 12,
+    resolution: 'low_resolution',
+    template: '<div class="carousel-cell" style="background-image:url({{image}})"><div class="cell-container"><div class="cell-textbox"><p>{{caption}}</p></div></div></div>',
+    after: function () {
+        var flickity = require('flickity');
+        var flickityOptions = {'contain': true, 'wrapAround': true };
+        var gallery = new flickity(document.getElementById('instafeed'), flickityOptions);        
+    }
+});
+feed.run();
 
     
 

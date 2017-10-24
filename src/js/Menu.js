36045873ebcb4b88c0e79043d0f2c1c1d7a833f8
Andres Hernandez <hernandez.andres.d@gmail.com>
2017-10-24 12:24:13 +1100

    loads menu-categories + menu-items and makes menu-item-info a react-state

--- src/js/Menu.js
+++ src/js/Menu.js
@@ -2,51 +2,69 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var createReactClass = require('create-react-class');
 
-var Menu = createReactClass({ 
+
+var MenuItem = createReactClass({ 
 	getInitialState() {
 		return {
-			menu: require('./data.js'),
-			view: false
+			viewInfo: false
 		}
-	}, 
+	},
 
 	toggle() {
-		(this.state.view) ? this.setState({ view: false }) : this.setState({ view: true });
-	}, 
-
-	renderItems(item) {
-		return (
-			<div key={ item.ref } id={ item.ref } className="grid-cell">
-                <div className="menu-item">
-                    <p className="item-title bold">{ item.name }</p>
-                    <p className="item-info clearfix">
-                    	<span className="pull-left">{ item.descrip }</span>
-                    	<span className="pull-right">+info</span>
-                    </p>
-                </div>
-            </div>
-		); 
+		(this.state.viewInfo) ? this.setState({ viewInfo: false }) : this.setState({ viewInfo: true });
 	},
+
+	renderDetail() {
+	    return (
+	    	<div className="clearfix">
+				<img className="pull--left" src={this.props.item.img} alt="this.props.item.name"/>
+				<div className="pull--right">
+					<p>{this.props.item.descrip}</p>
+					<p>{this.props.item.price}</p>
+				</div> 
+			</div>
+        )
+    },
+
+	render() {
+		return (
+			<div className="grid-cell">
+	    		<div className="menu__item" onClick={this.toggle}>
+	    			<div className="clearfix">
+	    				<p className="item__title bold pull--left">{ this.props.item.name }</p>
+    					<p className="item__info bold pull--right">{(this.state.viewInfo) ? '-': '+'} info</p>
+    				</div>
+					{ (this.state.viewInfo) && this.renderDetail() }
+				 </div>
+	        </div>
+		) 
+	}
+});
+
+var Menu = createReactClass({ 
 
 	renderCategories(category) {
 		return (
-			<article key={ category.id } id={ category.id } className="menu-category" onClick={this.toggle}>
-				<h1 className="category-header">{ category.category }</h1>
-				<div className="grid grid--gutters grid--center grid--1-1 grid--1-2@xs">
-					{ this.state.view && category.items.map(this.renderItems) }
+			<article 
+				key={ category.id } 
+				id={ category.id } 
+				className='menu__category' >
+
+				<h1 className='category__header'>{ category.category }</h1>
+				<div className="grid grid--gutters grid--1-1 grid--1-2@xs">
+					{ category.items.map( item => <MenuItem key={item.ref} id={item.ref} item={item} />)}
 				</div>
 			</article>
-		);
-	}, 
+		)
+	},
 
 	render() {
 		return (
 			<div>
-				{ this.state.menu.map( this.renderCategories ) }
+				{ this.props.list.map( this.renderCategories ) } 
 			</div>
 		); 
 	}
-
 }); 
 
-ReactDOM.render(<Menu />, document.getElementById('menu-component'));
+ReactDOM.render(<Menu list={ require('./data.js') } />, document.getElementById('menu-component'));
